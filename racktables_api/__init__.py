8c7a2ed9186cfc73f1ebd6936a54c1a4cc4d07ce
Julian Barnett <jbarnett@tableau.com>
2016-07-29 12:21:23 -0700

    restructuring and better db connection handling

--- racktables_api/__init__.py
+++ racktables_api/__init__.py
@@ -1,14 +1,49 @@
 # Imports
 import json
-from flask import Flask, jsonify, make_response
+import yaml
+from flask import Flask, jsonify, make_response, g
 from flask_httpauth import HTTPBasicAuth
 from racktables_api.main.views import main
 from racktables_api.hosts.views import hosts
+from racktables_api.database import is_number, RacktablesDB
+
+# Get connection details
+with open('config/db.yaml', 'r') as f:
+    config = yaml.load(f.read())
+    host = config['production']['host']
+    username = config['production']['username']
+    password = config['production']['password']
+    db = config['production']['db']
+
+# Connection handling function
+def get_db():
+    """
+    Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'RacktablesDB'):
+        g.RacktablesDB = RacktablesDB(host, username, password, db)
+        g.RacktablesDB.connect()
+    return g.RacktablesDB
 
 # Define the WSGI application object
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
+# Create database connection session before request if not exist
+@app.before_request
+def before_request():
+    g.RacktablesDB = RacktablesDB(host, username, password, db)
+    g.RacktablesDB.connect()
+
+# Close database connection session if not already closed
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'RacktablesDB', None)
+    if db is not None:
+        db.close()
+
+# Error handling
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'message': 'The request contained invalid data. Please try again.', 'status': '400'}), 400)
@@ -21,10 +56,11 @@ def not_found(error):
 def bad_request(error):
     return make_response(jsonify({'message': 'A resource with that name or ID already exists.', 'status': '409'}), 409)
 
-
 @app.errorhandler(410)
 def not_found(error):
     return make_response(jsonify({'message': 'A resource with that name cannot be found.', 'status': '410'}), 410)
+
+
 # Import a module / component using its blueprint handler variable
 app.register_blueprint(main, url_prefix='/')
 app.register_blueprint(hosts, url_prefix='/hosts')
--- racktables_api/hosts/views.py
+++ racktables_api/hosts/views.py
@@ -1,76 +1,38 @@
 # Import flask dependencies
 from flask import Blueprint, jsonify, g, request, make_response, abort
 
-# Import yaml for db config file parsing
-import yaml
-
-# Import database class
-from racktables_api.database import is_number, RacktablesDB
-
-# Get connection details
-with open('config/db.yaml', 'r') as f:
-    config = yaml.load(f.read())
-    host = config['production']['host']
-    username = config['production']['username']
-    password = config['production']['password']
-    db = config['production']['db']
 
 # Define the blueprint: 'api'
 hosts = Blueprint('hosts', __name__)
 
-# Create database connection session before request if not exist
-@hosts.before_request
-def before_request():
-    g.RacktablesDB = RacktablesDB(host, username, password, db)
-    g.RacktablesDB.connect()
 
-# Close database connection session if not already closed
-@hosts.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'RacktablesDB', None)
-    if db is not None:
-        db.close()
-
-# Connection handling function
-def get_db():
-    """
-    Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'RacktablesDB'):
-        g.RacktablesDB = RacktablesDB(host, username, password, db)
-        g.RacktablesDB.connect()
-    return g.RacktablesDB
 
 @hosts.route('/', methods=['GET'], strict_slashes=False)
 def get():
-    RacktablesDB = get_db()
-    result = RacktablesDB.get_all_objects()
+    result = g.RacktablesDB.get_all_objects()
     return jsonify(result)
 
 @hosts.route('/<string:hostname>', methods=['GET', 'PUT', 'POST', 'DELETE'], strict_slashes=False)
 def run(hostname):
-    RacktablesDB = get_db()
     if request.method == 'GET':
 
-        result = RacktablesDB.get(hostname)
+        result = g.RacktablesDB.get(hostname)
         return jsonify(result)
 
     elif request.method == 'PUT':
-        result = RacktablesDB.put(hostname, request.form)
+        result = g.RacktablesDB.put(hostname, request.form)
         return jsonify(result)
 
     elif request.method == 'POST':
-        result = RacktablesDB.post(hostname, request.form)
+        result = g.RacktablesDB.post(hostname, request.form)
         return jsonify(result)
 
     elif request.method == 'DELETE':
-        result = RacktablesDB.delete(hostname)
+        result = g.RacktablesDB.delete(hostname)
         return jsonify(result)
 
 @hosts.route('/<string:hostname>/comments', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def run1(hostname):
-    RacktablesDB = get_db()
     if request.method == 'GET':
-        result = RacktablesDB.get_comments(hostname)
+        result = g.RacktablesDB.get_comments(hostname)
         return jsonify(result)
